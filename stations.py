262b94f974f9669e1c327fbaa4df967850a8fb36
cnmicrobaby <microbaby@outlook.com>
2023-05-11 19:30:53 +0800

    Update stations.py

--- stations.py
+++ stations.py
@@ -8,9 +8,10 @@ import requests
 import urllib3
 
 urllib3.disable_warnings()
+
 url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9261'
 response = requests.get(url, verify=False)
-station = re.findall('([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
+station = re.findall('([\u4e00-\u9fa5]+)\\|([A-Z]+)', response.text)
 stations = dict(station)
 names = list(stations.keys())
 codes = list(stations.values())
