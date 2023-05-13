ff9edc59aa10a685745ed8dd33e5362c95844b23
cnmicrobaby <microbaby@outlook.com>
2023-05-13 10:57:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ import station as st
 import ticket as tk
 from log import logger
 
+# 载入各城市车站信息
 sts = st.getStations()
 
 if __name__ == '__main__':
+    # 判断输入的出发城市信息
     fromStation = input('请输入出发城市：')
     while fromStation not in list(sts.keys()):
         logger.info('输入的出发城市有误！')
         fromStation = input('请输入出发城市：')
+    # 判断输入的到达城市信息
     toStation = input('请输入到达城市：')
     while toStation not in list(sts.keys()):
         logger.info('输入的到达城市有误！')
         toStation = input('请输入到达城市：')
+    # 判断出发日期信息
     tripDate = input('（格式:2000-01-01，请输入大于等于今天的日期，且不超过15天）\n请输入出发日期：')
     while True:
         try:
@@ -38,9 +42,11 @@ if __name__ == '__main__':
             logger.info('输入的日期格式有误！')
             tripDate = input('（格式:2000-01-01，请输入大于等于今天的日期，且不超过15天）\n请输入出发日期：')
     try:
+        # 查询火车票信息
         traintickets = tk.getTrainTickets(fromStation, toStation, tripDate)
     except Exception as e:
         logger.error(e.args)
     else:
+        # 在控制台输出火车票信息
         trainlist = tk.TrainsCollection(traintickets)
         trainlist.pretty_print()
