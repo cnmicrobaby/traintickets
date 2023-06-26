84e2a60378a5b7ed93987776a134046c21999b84
cnmicrobaby <microbaby@outlook.com>
2023-06-26 17:24:14 +0800

    Update station.py

--- station.py
+++ station.py
@@ -10,7 +10,7 @@ import urllib3
 
 
 # 查询12306网站各城市站点对应信息
-def getStations():
+def get_stations():
     url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9261'
     urllib3.disable_warnings()
     response = requests.get(url, verify=False)
@@ -20,14 +20,14 @@ def getStations():
 
 
 # 根据站点代码查询站点名称
-def getName(sts, code):
+def get_name(sts, code):
     names = list(sts.keys())
     codes = list(sts.values())
     return names[codes.index(code)]
 
 
 # 根据站点名称查询站点代码
-def getCode(sts, name):
+def get_code(sts, name):
     names = list(sts.keys())
     codes = list(sts.values())
     return codes[names.index(name)]
